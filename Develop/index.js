d679f627bfca39d23412db24068e598ce6390167
tonialtendorf <tvohnoutka05@gmail.com>
2023-02-12 13:27:10 -0600

    added function to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,86 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 //test: const generateMarkdowns = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-questions
-  .prompt(
+const questions = []
+  inquirer.prompt(
+    [
     {
       type: 'input',
       message: 'What is the project title?',
       name: 'title',
     },
     {
-        type: 'input',
-        message: 'Provide a description of the application.',
-        name: 'description',
-      },
-      
-      {
+      type: 'input',
+      message: 'Provide a description of the application.',
+      name: 'description',
+    },
+
+    {
       type: 'input',
       message: 'Provide Installation instructions.',
       name: 'installation',
     },
     {
-        type: 'input',
-        message: 'Provide usage information.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'Provide contribution details.',
-        name: 'contribution',
-      },
-      {
-        type: 'input',
-        message: 'Provide test instructions',
-        name: 'test',
-      },
-      {
-        type: 'input',
-        message: 'Select license from the options:',
-        name: 'license',
-      },
-      {
-        type: 'input',
-        message: 'Please enter your github username.',
-        name: 'github',
-      },
-      {
-        type: 'input',
-        message: 'Please enter your email address.',
-        name: 'email',
-      },
-  )
-// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+      type: 'input',
+      message: 'Provide usage information.',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Provide contribution details.',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'Provide test instructions',
+      name: 'test',
+    },
+    {
+      type: 'list',
+      message: 'Select license from the options:',
+      name: 'license',
+      choices: ['None', 'GNU General Public License', 'MIT License', 'Boost Software License 1.0']
+    },
+    {
+      type: 'input',
+      message: 'Please enter your github username.',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email address.',
+      name: 'email',
+    },
+  ]);
 
 
+  // .then(({
+  //   title,
+  //   installation,
+  //   instructions,
+  //   credits,
+  //   license,
+  //   git,
+  //   email,
+  //   usage,
+  //   contributions,
 
-
- // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // }) => {
+  //   const template = 
+  // })
+   // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+ fs.writeFile(`./${fileName.join(' ')}.md`, data, (err) =>{
+  if(err){
+    console.log(err)
+  }
+  console.log("Your README file has been created");
+ })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -68,9 +88,3 @@ function init() {}
 // Function call to initialize app
 init();
 
-// example
-// fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-//   // TODO: Describe how this ternary operator works
-//   err ? console.error(err) : console.log('Commit logged!')
-// );
-// //if its an error then there will show the err or it will show commit logged
